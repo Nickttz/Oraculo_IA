c276285f10d226e3e06fc8e4e5c41f1fbd60327f
Nicolas <j-nicolaslima@hotmail.com>
2025-03-01 23:15:09 -0300

    Atualização no model do gemini

--- oracle.py
+++ oracle.py
@@ -13,7 +13,7 @@ load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
 # Criar modelo do Google Gemini
-llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=google_api_key)
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=google_api_key)
 
 # 🔹 Carregar a base de informações do CSV corretamente
 documents = []
